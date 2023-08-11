dc11d8efc7fc19fdfa60c58c20b1fe727558fa3c
Victoriabunmi72 <bhummhie97@gmail.com>
2023-08-11 01:53:05 +0100

    0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,22 +1,41 @@
 #!/usr/bin/python3
-
-
-"""
-    The letter H is the only character in a text file.
-    There are only two operations that your text editor can do on 
-	this file: Copy All and Paste. Write a method that calculates 
-	given a number n.
-    the least amount of processes necessary to produce exactly 
-	n H characters in the file.
-"""
+'''
+Calculated minimum number of operations required
+'''
 
 
 def minOperations(n):
-    nOpe = 0
-    minOpe = 2
-    while n > 1:
-        while n % minOpe == 0:
-            nOpe += minOpe
-            n /= minOpe
-        minOpe += 1
-    return nOpei
+    '''
+    Calculates the fewest number of operations required to achieve n
+    H characters
+
+    Args:
+        n: Number of H characters
+
+    Return: Fewest number of operations
+    '''
+    if not isinstance(n, int):
+        return 0
+    operations = 0  # Tracks number of operations
+    clipboard = 0   # Stores current number of characters
+    characters = 1  # Initial character H
+
+    while characters < n:
+        if clipboard == 0:
+            # first copy_all and paste
+            clipboard = characters
+            characters += clipboard
+            operations += 2
+
+        elif n - characters > 0 and (n - characters) % characters == 0:
+            # copy all and paste
+            clipboard = characters
+            characters += clipboard
+            operations += 2
+
+        elif clipboard > 0:
+            # paste
+            characters += clipboard
+            operations += 1
+
+    return operationsi
